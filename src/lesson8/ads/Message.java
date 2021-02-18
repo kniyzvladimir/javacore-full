4703de0847bfe4ad0569018411f01d521277e105
Volodymir Shpachnko <kniyzvladimir@gmail.com>
2021-02-19 01:39:05 +0200

    ads-3 les8

--- src/lesson8/ads/Message.java
+++ src/lesson8/ads/Message.java
@@ -3,18 +3,18 @@ package lesson8.ads;
 import java.util.Date;
 
 public class Message extends BaseEntity{
-    String message;
     User userFrom;
     User userTo;
     Date dateSent;
     Date dateRead;
+    String message;
 
-    public Message(long id, String message, User userFrom, User userTo, Date dateSent) {
+    public Message(long id, User userFrom, User userTo, String message) {
         super(id);
-        this.message = message;
         this.userFrom = userFrom;
         this.userTo = userTo;
         this.dateSent = new Date();
+        this.message = message;
     }
 
     void  setDateRead() {

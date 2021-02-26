04dbb920594bc732474ddf3d283cae4f56266ba8
Volodymir Shpachnko <kniyzvladimir@gmail.com>
2021-02-26 23:22:09 +0200

    ещё раз 9.3

--- src/lesson9/HomeWork/U3/Demo.java
+++ src/lesson9/HomeWork/U3/Demo.java
@@ -1,2 +1,19 @@
-package lesson9.HomeWork.U3;public class Demo {
+package lesson9.HomeWork.U3;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        User user1 = new User(654, "k", "kik");
+        User user2 = new User(4, "jk", "kk");
+        User user3 = new User(54, "kuyu", "kjmik");
+
+        User[] users = {null, null, null, null, user1, user2, user3};
+        UserRepository userRepository = new UserRepository(users);
+
+        System.out.println(userRepository.getUserBiId(654));
+        System.out.println(userRepository.getUserByName("jk"));
+        System.out.println(userRepository.getUserBySessionId("kj"));
+    }
+
 }
--- src/lesson9/HomeWork/U3/UserRepository.java
+++ src/lesson9/HomeWork/U3/UserRepository.java
@@ -1,2 +1,36 @@
-package lesson9.HomeWork.U3;public class UserRepository {
+package lesson9.HomeWork.U3;
+
+public class UserRepository {
+    private User[] users;
+
+    public UserRepository(User[] users) {
+        this.users = users;
+    }
+
+    public User getUserByName(String name) {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null && users[i].getName().equals(name)) {
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    public User getUserBiId(long id) {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null && users[i].getId() == id) {
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    public User getUserBySessionId(String sessionId) {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] !=null && users[i].getSessionId().equals(sessionId)) {
+                return users[i];
+            }
+        }
+        return null;
+    }
 }
